a2984aa488d7369a05e08cc07eb8c2c84751c1ea
Eyjolfur Kristjansson <eyjolfur12@ru.is>
2013-11-04 15:47:50 +0000

    First commit

---
+++ src/main/java/is/ru/pr_1/App.java
@@ -0,0 +1,13 @@
+package is.ru.pr_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/is/ru/pr_1/readme.txt
@@ -0,0 +1,1 @@
+Add some classes and shit here
